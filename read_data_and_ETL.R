2ddb3324338b755254d18315a52ce0be9b4df52f
mshiun_wu <mshiun.wu@gmail.com>
2018-11-06 16:20:30 +0800

    Update read_data_and_ETL.R

--- read_data_and_ETL.R
+++ read_data_and_ETL.R
@@ -22,9 +22,20 @@ if (length(new.pkgs)) {
 # Require packages
 suppressPackageStartupMessages(lapply(pkgs, library, character.only=T, lib.loc = .libPaths()[1]))
 
-#-------------------------------------------------------#
-setwd('E:/Marvin.Wu/myProject/prj_mentor')
 
+# install.packages('AAA')
+# library(AAA)
+# require(AAA)
+
+
+#-------------------------------------------------------#
+
+
+test1 <- read_xls(path = 'E:/Marvin.Wu/myProject/prj_mentor/中保產品分析用_R.xls', sheet = 'Sheet1')
+
+
+
+setwd('E:/Marvin.Wu/myProject/prj_mentor')
 test1 <- read_xls('中保產品分析用_R.xls', sheet = 'Sheet1')
 test2 <- read_xls('中保產品分析用_R.xls', sheet = 'Sheet2')
 test3 <- read_xls('中保產品分析用_R.xls', sheet = 'Sheet3')
@@ -32,5 +43,23 @@ test4 <- read_xls('中保產品分析用_R.xls', sheet = 'Sheet4')
 test5 <- read_xls('中保產品分析用_R.xls', sheet = 'Sheet5')
 test6 <- read_xls('中保產品分析用_R.xls', sheet = 'Sheet6')
 test7 <- read_xls('中保產品分析用_R.xls', sheet = 'Sheet7')
+
+raw_data <- bind_rows(test1, test2, test3, test4, test5, test6, test7)
+
+# 取出器材編號與器材名稱表
+prod_name <- raw_data %>% count(器材名稱)
+
+
+
+# 有多少契約編號
+raw_data %>% select(契約編號) %>% distinct()
+raw_data %>% pull(契約編號) %>% unique() %>% length()
+raw_data %>% count(契約編號)  
+
+# 次數統計
+tb1 <- raw_data %>% count(., 契約編號, 系統別) %>% count(., 系統別) %>% arrange(., desc(nn)) %>% mutate(prob = nn/sum(nn))
+
+
+
 
 
